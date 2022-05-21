587c362bcfb2b1e4c2635c52b1801bde4bf8bbb2
vindr22 <105640722+vindr22@users.noreply.github.com>
2022-05-21 23:56:58 +0700

    Create 21.05.2022.py

---
+++ 21.05.2022.py
@@ -0,0 +1,18 @@
+full_name = input('what is your full name? ')
+print('hi ' + full_name)
+
+birth_year = input('what is your birth year? ')
+int(birth_year)
+age = 2022-int(birth_year)
+print(age)
+
+#int for integer
+#float for decimals
+#bool to convert a value into boolean
+#str to convert a value to a string
+
+
+first = input('what is your first number? ')
+sec = input('what is your second number? ')
+sum = float(first)+float(sec)
+print('The sum is ' + str(sum))
