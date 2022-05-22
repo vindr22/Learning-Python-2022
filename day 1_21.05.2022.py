9fc62ffc8c9b7f08f230189f60966fd058c7f113
vindr22 <105640722+vindr22@users.noreply.github.com>
2022-05-22 11:58:13 +0700

    Update day 1_21.05.2022.py

--- day 1_21.05.2022.py
+++ day 1_21.05.2022.py
@@ -6,13 +6,13 @@ int(birth_year)
 age = 2022-int(birth_year)
 print(age)
 
-#int for integer
-#float for decimals
-#bool to convert a value into boolean
-#str to convert a value to a string
+# int for integer
+# float for decimals
+# bool to convert a value into boolean
+# str to convert a value to a string
 
 
 first = input('what is your first number? ')
 sec = input('what is your second number? ')
-sum = float(first)+float(sec)
+sum = float(first) + float(sec)
 print('The sum is ' + str(sum))
